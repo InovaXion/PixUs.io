215837049016dffa713bf541860c21751d48cf91
InovaXion <mmghaoues@gmail.com>
2019-04-19 09:51:58 +0200

    test

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,12 +1,109 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>PixUs</title>
 </head>
-<body>
-    <h1>Salut mon ami !!</h1>
+
+<body id="page-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+        <div class="container">
+            <a class="navbar-brand js-scroll-trigger" href="#page-top">Start Bootstrap</a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                Menu
+                <i class="fas fa-bars"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav text-uppercase ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="#services">Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="#about">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="#team">Team</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- Header -->
+    <header class="masthead">
+        <div class="container">
+            <div style="padding-top: 150px">
+                <article class="col-xs-12 maincontent">
+                    <header class="page-header">
+                        <h1 class="page-title">Registration</h1>
+                        <div class="row col-12 align-item-center justify-content-center">
+                            <div class="col-6 form-group">
+                                <div class="panel panel-default">
+                                    <h3 class="thin text-center">Register a new account</h3>
+                                    <p class="text-center text-muted">Lorem ipsum dolor sit amet, <a href="signin.html">Login</a>
+                                        adipisicing elit. Quo nulla quibusdam cum doloremque incidunt nemo sunt a tenetur omnis odio. </p>
+                                    <hr>
+                                    <h1>test</h1>
+                                    <form>
+                                        <div class="top-margin">
+                                            <label>First Name</label>
+                                            <input type="text" class="form-control">
+                                        </div>
+                                        <div class="top-margin">
+                                            <label>Last Name</label>
+                                            <input type="text" class="form-control">
+                                        </div>
+                                        <div class="top-margin">
+                                            <label>Email Address <span class="text-danger">*</span></label>
+                                            <input type="text" class="form-control">
+                                        </div>
+
+                                        <div class="row top-margin">
+                                            <div class="col-sm-6">
+                                                <label>Password <span class="text-danger">*</span></label>
+                                                <input type="text" class="form-control">
+                                            </div>
+                                            <div class="col-sm-6">
+                                                <label>Confirm Password <span class="text-danger">*</span></label>
+                                                <input type="text" class="form-control">
+                                            </div>
+                                        </div>
+
+                                        <hr>
+
+                                        <div class="row">
+                                            <div class="col-lg-8">
+                                                <label class="checkbox">
+                                                    <input type="checkbox">
+                                                    I've read the <a href="page_terms.html">Terms and Conditions</a>
+                                                </label>
+                                            </div>
+                                            <div class="col-lg-4 text-right">
+                                                <button class="btn btn-action" type="submit">Register</button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </header>
+                </article>
+            </div>
+        </div>
+    </header>
 </body>
+
 </html>
